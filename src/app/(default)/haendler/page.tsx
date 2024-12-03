12110cc11d576685b0be76692c8e77b830093e20
Thomas Dietrich <dietrich.tom@gmail.com>
2024-12-03 21:46:43 +0100

    pt

--- src/app/(default)/haendler/page.tsx
+++ src/app/(default)/haendler/page.tsx
@@ -1,10 +1,16 @@
 import Item from "@/components/features/dealer/search/Item";
 import { Dealer } from "@/types/DealerSearchApiResponse";
 import { fetchDealers } from "@/utils/fetch-dealers";
+import { headers } from "next/headers";
 import React from "react";
 
 export default async function Page() {
-  const dealers = await fetchDealers();
+  const headersList = await headers();
+  const subdomain = headersList.get("x-subdomain") || "default";
+
+  const pt = subdomain === "wl-lc" ? "v4Lkda9cF2lzqpnwrwoq" : 0;
+
+  const dealers = await fetchDealers(pt);
 
   return (
     <div className="p-8">
--- src/utils/fetch-dealers.ts
+++ src/utils/fetch-dealers.ts
@@ -1,8 +1,10 @@
 import { DealerSearchApiResponse } from "@/types/DealerSearchApiResponse";
 
-export const fetchDealers = async (): Promise<DealerSearchApiResponse> => {
+export const fetchDealers = async (
+  pt: string
+): Promise<DealerSearchApiResponse> => {
   const response = await fetch(
-    `https://api.meinereifen.at/haendler/search?pt=0`
+    `https://api.meinereifen.at/haendler/search?pt=${pt}`
   );
   const dealers = await response.json();
   return dealers;
