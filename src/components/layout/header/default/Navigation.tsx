48d81cfe81c25cfa8f5a5b5648b7085b0ac1e54b
Thomas Dietrich <dietrich.tom@gmail.com>
2024-12-03 21:28:35 +0100

    navigation

--- src/components/layout/header/default/Navigation.tsx
+++ src/components/layout/header/default/Navigation.tsx
@@ -6,12 +6,14 @@ export default function Navigation() {
   return (
     <div className="bg-red-200 w-full p-8 flex flex-row justify-between">
       <div>
-        <Image
-          src="/images/meinereifen-logo.svg"
-          alt="Logo"
-          width={196}
-          height={40}
-        />
+        <Link href="/" className="cursor-pointer">
+          <Image
+            src="/images/meinereifen-logo.svg"
+            alt="Logo"
+            width={196}
+            height={40}
+          />
+        </Link>
       </div>
       <ul className="flex flex-row gap-4">
         <li className="hover:underline cursor-pointer">
