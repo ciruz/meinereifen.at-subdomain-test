756597347f7757aaeed76e48776dc487015b3f97
Thomas Dietrich <dietrich.tom@gmail.com>
2024-12-03 16:30:24 +0100

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,13 @@
 // app/layout.tsx
-import { cookies } from "next/headers";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const cookieStore = cookies();
-  const subdomain = cookieStore.get("x-subdomain") || "default";
-
   return (
     <html lang="en">
-      <body>
-        <h1>Subdomain: {subdomain}</h1>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
