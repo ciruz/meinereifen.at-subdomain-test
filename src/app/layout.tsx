68145a2db79a5f918bcbfa3806858d5f77aaa966
Thomas Dietrich <dietrich.tom@gmail.com>
2024-12-03 16:49:34 +0100

    headers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,20 @@
 // app/layout.tsx
 
-import { cookies } from "next/headers";
+import { headers } from "next/headers";
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const cookieStore = await cookies();
-  const subdomain = cookieStore.get("x-subdomain")?.value || "default";
+  const headersList = await headers();
+  const subdomain = headersList.get("x-subdomain") || "default";
 
   return (
     <html lang="en">
       <body>
         subdomain in layout: {subdomain}
         {children}
-        {cookieStore.getAll().map((cookie) => (
-          <div key={cookie.name}>
-            <p>Name: {cookie.name}</p>
-            <p>Value: {cookie.value}</p>
-          </div>
-        ))}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import { cookies } from "next/headers";
+import { headers } from "next/headers";
 
 export default async function Page() {
-  const cookieStore = await cookies();
-  const subdomain = cookieStore.get("x-subdomain")?.value || "default";
+  const headersList = await headers();
+  const subdomain = headersList.get("x-subdomain") || "default";
 
   return <div>subdomain in page: {subdomain}</div>;
 }
